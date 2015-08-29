6189fbb4c2b150182792f0f1ce061d414990dd9d
acvdvyve <vandevyver.andreas@outlook.com>
2015-08-29 12:11:01 +0200

    set symlink database.yml, cleaned up code

--- config/deploy.rb
+++ config/deploy.rb
@@ -6,31 +6,36 @@ set :repo_url, 'git@github.ugent.be:vgk/hestia.git'
  set :deploy_to, '/var/www/hestia'
  set :scm, :git
  set :branch, "master"
- set :user, "hestia"
+ set :deploy_user, "hestia"
  set :use_sudo, false
  set :rails_env, "production"
- # set :deploy_via, :copy
+# setup rvm
+ set :rbenv_type, :system
  set :rbenv_ruby, '2.1.2'
+ set :rbenv_prefix, "RBENV_ROOT=#{fetch(:rbenv_path)} RBENV_VERSION=#{fetch(:rbenv_ruby)} #{fetch(:rbenv_path)}/bin/rbenv exec"
+ set :rbenv_map_bins, %w{rake gem bundle ruby rails}
+# authorisation method
  set :ssh_options, { :forward_agent => true }
-set :passenger_roles, :app
-set :passenger_restart_runner, :sequence
-set :passenger_restart_wait, 5
-set :passenger_restart_limit, 2
-set :passenger_restart_with_sudo, true
-set :passenger_restart_with_touch, true
-set :passenger_environment_variables, {}
-set :passenger_restart_command, 'passenger-config restart-app'
-set :passenger_restart_options, -> { "#{deploy_to} --ignore-app-not-running" }
+# passenger-capistrano gem configuration. see https://github.com/capistrano/passenger/ for more info.  
+ set :passenger_roles, :app
+ set :passenger_restart_runner, :sequence
+ set :passenger_restart_wait, 5
+ set :passenger_restart_limit, 2
+ set :passenger_restart_with_sudo, true
+ set :passenger_restart_with_touch, true
+ set :passenger_environment_variables, {}
+ set :passenger_restart_command, 'passenger-config restart-app'
+ set :passenger_restart_options, -> { "#{deploy_to} --ignore-app-not-running" }
 # default_run_options[:pty] = true
 server "vgkserv.ugent.be", :roles => [:app, :web, :db], :primary => true
 
 
 # set :format, :pretty
 # set :log_level, :debug
-# set :pty, true
+ set :pty, true
 
-# set :linked_files, %w{config/database.yml}
-# set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
+ set :linked_files, %w{config/database.yml}
+ set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
 
 # set :default_env, { path: "/opt/ruby/bin:$PATH" }
  set :keep_releases, 5
@@ -47,11 +52,6 @@ namespace :deploy do
     end
   end
 
-#  desc "Restart Passenger app"
-#  task :restart do
-#    execute :try_sudo, " touch #{ File.join(current_path, 'tmp', 'restart.txt') }"
-#  end
-
   after :restart, :clear_cache do
     on roles(:web), in: :groups, limit: 3, wait: 10 do
       # Here we can do anything such as:
@@ -62,19 +62,8 @@ namespace :deploy do
   end
 
   after :finishing, 'deploy:cleanup'
-#  after "deploy", "deploy:symlink_config_files"
   after "deploy", "deploy:restart"
 
 end
 
-desc "Symlink shared config files"
-task :symlink_config_files do
-  on roles (:all) do	
-	execute "ln -s #{ deploy_to }/shared/config/database.yml ##{ current_paht }/config/database.yml"
-  end
-end
 
-#desc "Restart Passenger app"
-#task :restart do
-#	run "#{ try_sudo } touch #{ File.join(current_path, 'tmp', 'restart.txt') }"
-#end
