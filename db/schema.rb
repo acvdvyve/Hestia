f3257d5c3a5a4e05fb0c4b345e474a744500ba1f
Benjamin Rombaut <benjamin.rombaut@gmail.com>
2016-01-17 12:31:42 +0100

    no unique email

--- db/schema.rb
+++ db/schema.rb
@@ -158,7 +158,7 @@ ActiveRecord::Schema.define(version: 20150821203447) do
     t.string   "cas_uid"
   end
 
-  add_index "users", ["email"], name: "index_users_on_email", unique: true
+  add_index "users", ["email"], name: "index_users_on_email"
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
 
 end
